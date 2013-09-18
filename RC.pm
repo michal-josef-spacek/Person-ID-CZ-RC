991353d354509ce8421b27f7a0d2e60451516ddd
Michal Špaček <tupinek@gmail.com>
2013-09-18 10:47:44 +0200

    Add doc.

--- RC.pm
+++ RC.pm
@@ -171,3 +171,160 @@ sub _parse {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Person::ID::CZ::RC - Perl class for Czech RC identification.
+
+=head1 SYNOPSIS
+
+ use Person::ID::CZ::RC;
+ my $obj = Person::ID::CZ::RC->new(%params);
+ my $alternate = $obj->alternate;
+ my $checksum = $obj->checksum;
+ my $is_valid = $obj->is_valid;
+ my $month = $obj->month;
+ my $rc = $obj->rc;
+ my $serial = $obj->serial;
+ my $sex = $obj->sex;
+ my $year = $obj->year;
+
+=head1 METHODS
+
+=over 8
+
+=item C<new(%params)>
+
+ Constructor.
+
+=over 8
+
+=item * C<rc>
+
+ Input Czech RC identification.
+ It is required.
+
+=back
+
+=item C<alternate()>
+
+ Get flag, that means alternate RC identification.
+ Returns 0/1.
+
+=item C<checksum()>
+
+ Get checksum.
+ Returns string with one number character or '-'.
+
+=item C<day()>
+
+ Get day of birth.
+ Returns string with day.
+
+=item C<is_valid()>
+
+ Get flag, that means validity of rc identification.
+ Returns 0/1.
+
+=item C<month()>
+
+ Get month of birth.
+ Returns string with month.
+
+=item C<rc()>
+
+ Get rc identification.
+ Returns string with rc identification.
+
+=item C<serial()>
+
+ Get serial part of rc identification.
+ Returns string with three numbers.
+
+=item C<sex()>
+
+ Get flag, that means sex of person.
+ Returns male/female string.
+
+=item C<year()>
+
+ Get year of birth.
+ Returns string with year.
+
+=back
+
+=head1 ERRORS
+
+ new():
+         Parameter 'rc' is required.
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
+
+=head1 EXAMPLE
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Person::ID::RC::CZ;
+
+ # Object.
+ my $obj = Person::ID::CZ::RC->new(
+         'rc' => '840501/1330',
+ );
+
+ # Print out.
+ print "Personal number: ".$rc->rc."\n";
+ print "Year: ".$rc->year."\n";
+ print "Month: ".$rc->month."\n";
+ print "Day: ".$rc->day."\n";
+ print "Sex: ".$rc->sex."\n";
+ print "Serial: ".$rc->serial."\n";
+ print "Checksum: ".$rc->checksum."\n";
+ print "Alternate: ".$rc->alternate."\n";
+
+ # Output:
+ # Personal number: 840501/1330
+ # Year: 1984
+ # Month: 05
+ # Day: 01
+ # Sex: male
+ # Serial: 133
+ # Checksum: 0
+ # Alternate: 0
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<Error::Pure>.
+
+=head1 SEE ALSO
+
+L<Business::DK::CPR>,
+L<No::PersonNr>,
+L<Se::PersonNr>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Person::ID::CZ::RC>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+BSD license.
+
+=head1 VERSION
+
+0.01
+
+=cut
