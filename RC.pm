282401e51ea97d5a5f172e15a84efe6b487e4787
Michal Špaček <tupinek@gmail.com>
2013-09-18 11:19:13 +0200

    Add examples to doc.

--- RC.pm
+++ RC.pm
@@ -387,6 +387,129 @@ Person::ID::CZ::RC - Perl class for Czech RC identification.
  # Valid: 0
  # Error: Date isn't valid.
 
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Person::ID::CZ::RC;
+
+ # Object.
+ my $obj = Person::ID::CZ::RC->new(
+         'rc' => '840229/1330',
+ );
+
+ # Get error.
+ my $error = $obj->error || '-';
+
+ # Print out.
+ print "Personal number: ".$obj->rc."\n";
+ print "Year: ".$obj->year."\n";
+ print "Month: ".$obj->month."\n";
+ print "Day: ".$obj->day."\n";
+ print "Sex: ".$obj->sex."\n";
+ print "Serial: ".$obj->serial."\n";
+ print "Checksum: ".$obj->checksum."\n";
+ print "Alternate: ".$obj->alternate."\n";
+ print "Valid: ".$obj->is_valid."\n";
+ print "Error: ".$error."\n";
+
+ # Output:
+ # Personal number: 840229/1330
+ # Year: 1984
+ # Month: 02
+ # Day: 29
+ # Sex: male
+ # Serial: 133
+ # Checksum: 0
+ # Alternate: 0
+ # Valid: 0
+ # Error: Checksum isn't valid.
+
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Person::ID::CZ::RC;
+
+ # Object.
+ my $obj = Person::ID::CZ::RC->new(
+         'rc' => '840229/133',
+ );
+
+ # Get error.
+ my $error = $obj->error || '-';
+
+ # Print out.
+ print "Personal number: ".$obj->rc."\n";
+ print "Year: ".$obj->year."\n";
+ print "Month: ".$obj->month."\n";
+ print "Day: ".$obj->day."\n";
+ print "Sex: ".$obj->sex."\n";
+ print "Serial: ".$obj->serial."\n";
+ print "Checksum: ".$obj->checksum."\n";
+ print "Alternate: ".$obj->alternate."\n";
+ print "Valid: ".$obj->is_valid."\n";
+ print "Error: ".$error."\n";
+
+ # Output:
+ # Personal number: 840229/133
+ # Year: 1984
+ # Month: 02
+ # Day: 29
+ # Sex: male
+ # Serial: 133
+ # Checksum: -
+ # Alternate: 0
+ # Valid: 0
+ # Error: Format of rc identification hasn't checksum.
+
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Person::ID::CZ::RC;
+
+ # Object.
+ my $obj = Person::ID::CZ::RC->new(
+         'rc' => '840229|1330',
+ );
+
+ # Get error.
+ my $error = $obj->error || '-';
+
+ # Print out.
+ print "Personal number: ".$obj->rc."\n";
+ print "Year: ".$obj->year."\n";
+ print "Month: ".$obj->month."\n";
+ print "Day: ".$obj->day."\n";
+ print "Sex: ".$obj->sex."\n";
+ print "Serial: ".$obj->serial."\n";
+ print "Checksum: ".$obj->checksum."\n";
+ print "Alternate: ".$obj->alternate."\n";
+ print "Valid: ".$obj->is_valid."\n";
+ print "Error: ".$error."\n";
+
+ # Output:
+ # Personal number: 840229|1330
+ # Year: -
+ # Month: -
+ # Day: -
+ # Sex: -
+ # Serial: -
+ # Checksum: -
+ # Alternate: -
+ # Valid: 0
+ # Error: Format of rc identification isn't valid.
+
 =head1 DEPENDENCIES
 
 L<Class::Utils>,
